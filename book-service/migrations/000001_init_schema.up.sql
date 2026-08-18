abf4a0c651c3fe73951ab9bf5e3d994186fb61bb
artemiyulyanov <artemiyulyanov.career@gmail.com>
2026-08-19 00:02:38 +0300

    feat: schema changed

--- book-service/migrations/000001_init_schema.up.sql
+++ book-service/migrations/000001_init_schema.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(200) NOT NULL,
-    author VARCHAR(100) NOT NULL /* масштабирование до REFERENCES authors(id) */
+    author_id BIGINT NOT NULL
 );
