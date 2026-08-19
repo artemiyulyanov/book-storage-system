6d65a97469c55d59abef3fd1608d98d855535ded
artemiyulyanov <artemiyulyanov.career@gmail.com>
2026-08-19 13:40:21 +0300

    feat: GET /users, GET /users/{id} implemented

--- user-service/internal/database/repository/repository.go
+++ user-service/internal/database/repository/repository.go
@@ -1,9 +1,46 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"common/models"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type UserRepository struct {
 	pool *pgxpool.Pool
+}
+
+func (repo *UserRepository) GetUsers(ctx context.Context) ([]models.User, error) {
+	rows, err := repo.pool.Query(ctx, "SELECT * FROM users ORDER BY id")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.User])
+
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (repo *UserRepository) GetUser(ctx context.Context, id int64) (*models.User, error) {
+	var user models.User
+
+	err := repo.pool.QueryRow(ctx, "SELECT id, first_name, last_name, email, password_hash FROM users WHERE id=$1", id).
+		Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.PasswordHash)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func NewUserRepository(pool *pgxpool.Pool) *UserRepository {
--- user-service/internal/handlers/handlers.go
+++ user-service/internal/handlers/handlers.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"database/sql"
+	"errors"
+	"net/http"
 	"user-service/internal/database/repository"
+
+	"common/network"
 
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,15 +16,47 @@ type UserHandlers struct {
 	repo *repository.UserRepository
 }
 
+func (handlers *UserHandlers) getUsers(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	users, err := handlers.repo.GetUsers(ctx)
+
+	if err != nil {
+		network.RespondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	network.RespondJSON(w, http.StatusOK, users)
+}
+
+func (handlers *UserHandlers) getUser(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	id, err := network.ParseID(r)
+
+	if err != nil {
+		network.RespondError(w, http.StatusBadRequest, "Incorrect id!")
+		return
+	}
+
+	user, err := handlers.repo.GetUser(ctx, id)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			network.RespondError(w, http.StatusNotFound, "User not found!")
+		} else {
+			network.RespondError(w, http.StatusBadRequest, err.Error())
+		}
+
+		return
+	}
+
+	network.RespondJSON(w, http.StatusOK, user)
+}
+
 func (handlers *UserHandlers) registerRoutes(router *mux.Router) {
-	// router.HandleFunc("/books", handlers.getBooks).Methods("GET")
-	// router.HandleFunc("/books/{id}", handlers.getBook).Methods("GET")
-
-	// router.HandleFunc("/books", handlers.createBook).Methods("POST")
-
-	// router.HandleFunc("/books/{id}", handlers.updateBook).Methods("PUT")
-
-	// router.HandleFunc("/books/{id}", handlers.deleteBook).Methods("DELETE")
+	router.HandleFunc("/users", handlers.getUsers).Methods("GET")
+	router.HandleFunc("/users/{id}", handlers.getUser).Methods("GET")
 }
 
 func RegisterUserHandlers(router *mux.Router, pool *pgxpool.Pool) *UserHandlers {
